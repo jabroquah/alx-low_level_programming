96c5e420260bdd43aeae3c7d8632546a1af35669
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-10 03:40:41 +0000

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (sucess)
+*
+*
+*/
+
+int main(void)
+{
+	int i;
+	
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i == 57)
+			break;
+		putchar(44);
+		putchar(32);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
