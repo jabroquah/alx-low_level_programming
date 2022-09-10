9bcdaa0559e7561a5ebfc52c617334ec36a241f6
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-10 03:38:50 +0000

    Added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (sucess)
+*
+*
+*/
+
+int main(void)
+{
+	int i;
+	
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
