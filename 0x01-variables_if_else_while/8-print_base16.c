dd9ddc82b268e8f14dcf122b40e94d9a64365ac6
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-10 03:40:24 +0000

    Added 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (sucess)
+*
+*
+*/
+
+int main(void)
+{
+	int i;
+	
+	for (i = 48; i <= 102; i++)
+	{
+		putchar(i);
+		if (i == 57)
+		{
+			i = 97;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
