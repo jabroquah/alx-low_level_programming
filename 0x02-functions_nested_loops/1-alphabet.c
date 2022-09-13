72e3743eb34d5f38e790f65c890a67bb0f2b68a2
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-13 23:50:29 +0000

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (sucess)
+*
+*
+*/
+
+int main(void)
+{
+	int n;
+
+	for(n = 97; n <= 122; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
