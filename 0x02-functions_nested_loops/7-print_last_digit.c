212fbebb4ea8373667bc4145e5589b2c7920c694
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-14 04:31:57 +0000

    Updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,18 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = n % 10;
-	
-	putchar(last_digit + '0');
+	if (n > 0)
+	{	
+		last_digit = n % 10;
+		putchar(last_digit + '0');
+		return last_digit;
+	}
+	else
+	{
+		last_digit = (-1 * n) % 10;
+		putchar(last digit + '0');
+		return last_digit;
+	{
 
-	return last_digit;
+
 }
