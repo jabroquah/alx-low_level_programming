8732b7a07557c8cd58cda7aba0ef1be782f47ec8
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-14 01:32:10 +0000

    Initial commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+/**
+* print_sign(int n) - prints the sign of a number
+* @c: character to be checked
+* Return: 1 if c is > 0, 
+* 0 if c == 0 and 
+* -1 if c < 0
+*
+*/
+
+
+int _print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar('-');	
+		return (-1);
+	}
+	else
+		return(0);
+}
