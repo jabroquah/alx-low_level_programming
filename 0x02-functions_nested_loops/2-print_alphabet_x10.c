bfb0d6dce5f9224b1eb4273f3fac2bb8ff5862e2
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-14 00:10:26 +0000

    Added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* print_alphabet_x10 - prints 10 times the alphabet
+*
+*
+*/
+
+void print_alphabet_x10(void)
+{
+	int a,b;
+
+	for (a = 1; a <=2; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			putchar(n);
+			putchar('\n');
+		}
+	}
+}
