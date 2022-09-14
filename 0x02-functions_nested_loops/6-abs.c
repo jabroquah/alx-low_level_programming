f5dd200256984489b34fd0fe46c76eeb8a1c0608
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-14 03:18:25 +0000

    Updated

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* _abs - prints the sign of a number
+* @n: the value to be calculated
+* Return: the absolute value 
+*
+*/
+
+
+int _print_sign(int n)
+{
+	return (abs(n);
+
+}
