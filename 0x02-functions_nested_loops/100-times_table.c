2552907b63a0f0c71861b1a9d364f182f0211e44
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-15 02:49:29 +0000

    Updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,15 +22,6 @@ void print_times_table(int n)
 	{
 		for (a = 0; a <= n; a++)
 		{
-			if (a * b >= 100)
-			{
-				hundreds = a * b / 100;
-				units = a * b % 10;
-				tens = a * b / 10;
-				_putchar(hundreds + '0');
-				_putchar(tens + '0');
-				_putchar(units + '0');
-			}
 			if (a * b >= 10 && a * b < 100)
 			{
 				units = a * b % 10;
@@ -46,7 +37,7 @@ void print_times_table(int n)
 			if (a != n)
 			{
 				_putchar(',');
-				_putchar(9);
+				_putchar(' ');
 			}
 		}
 		a = 0;
