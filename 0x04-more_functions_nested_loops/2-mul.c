29edd2963c3fa2442a9adfdaf4185213c7d732cb
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-16 02:36:17 +0000

    Initial commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two numbers
+ * @num1: This is multiplicand
+ * @num2: This is multiplier
+ *
+ * Return: The product
+ */
+
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
