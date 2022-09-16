21f72608b44950eaefdf6519380caf0abeae1950
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-16 03:02:37 +0000

    Initial commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - Prints # n*n times
+ * @n: describes the size of # printed
+ *
+ * Return: void
+ */
+
+
+void print_square(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
