ab9620ff646ad5f584ff0207281d1c4f996e897a
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-16 02:48:21 +0000

    Initial commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - Prints '_' n times
+ * @n: describes  how many times _ is printed
+ *
+ * Return: Null Void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
