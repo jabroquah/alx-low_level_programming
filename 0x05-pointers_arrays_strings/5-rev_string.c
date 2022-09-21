d230d3759b404a43ad2e66b9ab89c5ec5f92e315
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-21 03:47:27 +0000

    Initial commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include "2-strlen.c"
+
+
+/**
+* rev_string - reverses a string
+*
+* @a: pointer to string
+*
+*/
+
+void rev_string(char *a)
+{
+
+	int backIndex, startIndex, length, mid, temp;
+
+	length = _strlen(a)
+	startIndex = 0;
+	backIndex = length - 1;
+
+	if (length != 0)
+	{
+		mid = length % 2 == 1 ? (length-1) / 2 : length / 2;
+	
+		for (start = 0; start <= mid; backIndex--, startIndex++)
+		{
+			temp = *(a + startIndex);
+			*(s + start) = *(s + backIndex);
+			*(s + backIndex) = temp; 
+		}
+	}
+
+
+	putchar('\n');
+}
