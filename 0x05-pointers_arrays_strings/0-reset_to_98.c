c45cd2631f6e0eec7908ebc0111c64c1f9345956
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-21 00:26:55 +0000

    Updated

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,10 +5,11 @@
 /**
 * reset_to_98 - takes a pointer and updates its value
 *
-* @*n: pointer to an int
+* @n: pointer to an int
 */
 
-void reset_to_98(int *n){
-	*n = 402;
+void reset_to_98(int *n)
+{
+	*n = 98;
 	_putchar('\n');
 }
