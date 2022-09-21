74c4a8a379314a17ac3e93187e0a4f01b53b8024
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-21 00:14:04 +0000

    Initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+* reset_to_98 - takes a pointer and updates its value
+*
+* @*n: pointer to an int
+*/
+
+void reset_to_98(int *n){
+	*n = 402;
+	_putchar(10);
+}
