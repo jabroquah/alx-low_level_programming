06e48e0ae7626d6a8990f18d6f08ac26b8ee9265
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-23 04:10:49 +0000

    Updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,8 +13,10 @@ int _strcmp(char *s1, char *s2)
 	int index = 0, difference = 0; 
 
 
-	while (difference == 0 && *(s1 + index) != '\0' && *(s2 + index != '\0')
+	while (difference == 0)
 	{
+		if ((*(s1 + index) == '\0') && (*(s2 + index) == '\0'))
+			break;
 		difference = *(s1 + index) - *(s2 + index);
 		index++;
 	}
