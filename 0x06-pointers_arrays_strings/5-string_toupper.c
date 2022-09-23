c2df9eb0f13b525e5f147333e43f65336a20024c
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-23 04:43:09 +0000

    Updated

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,9 @@
 
 char *string_toupper(char *n)
 {
-	int a = 0;
+	int a;
 
-	for (; a < *(n + a) != '\0'; a++)
+	for (a = 0; *(n + a) != '\0'; a++)
 	{
 		if (*(n + a) >= 97 && *(n + a) <= 122)
 			*(n + a) = *(n + a) - 32;
