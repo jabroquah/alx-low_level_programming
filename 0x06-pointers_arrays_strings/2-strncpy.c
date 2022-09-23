197f26f12016dff5fcf7d92b0b8335e07bad52c7
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-23 04:05:06 +0000

    Updated

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index;
+	int index = 0;
 
-	for (index = 0; index < n && *(src + index) != '\0'; index++)
+	for (; index < n && *(src + index) != '\0'; index++)
 	{
 		*(dest + index) = *(src + index);
 	}
 
-	*(dest + index) = '\0';
+	for (; index < n; index++)
+		*(dest + index) = '\0';
 
 	return (dest);
 }
