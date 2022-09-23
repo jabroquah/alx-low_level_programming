fa1b942781ec6ad29509dc422f4940f0d7ebcc48
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-23 04:45:22 +0000

    Updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,14 +6,15 @@
  *
  * Return: string with every first letter capitalized
  */
+
 char *cap_string(char *s)
 {
 	int wordSeparators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 	int stringIndex = 0, separatorIndex;
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
+	if (*(s + stringIndex) >= 97 && *(s + stringIndex) <= 122)
+		*(s + stringIndex) = *(s + stringIndex) - 32;
+	stringIndex;
 
 	while (*(s + stringIndex) != '\0')
 	{
