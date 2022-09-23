c19c8dc71c8b7507e56737e9262a59297d7f4b62
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-23 02:05:12 +0000

    Initial commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - Concatenates two strings
+ *
+ * @dest: string to return
+ * @src: string to join
+ *
+ * Return: A pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int destIndex = 0;
+	int srcIndex = 0;
+
+	/** Terminating null byte considered */
+	while (*(dest + destIndex) != '\0')
+	{
+		destIndex++;
+	}
+
+	for (srcIndex = 0; srcIndex <= (strlen(src) - 1) && *(src + srcIndex) != '\0'; srcIndex++)
+	{
+		*(dest + destIndex + srcIndex) = *(src + srcIndex);
+	}
+
+	*(dest + destIndex + srcIndex) = '\0';
+
+	return (dest);
+}
