9c5ce5147cf6cd57555ac97a0cfd76ef6fe5a025
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-28 14:42:25 +0000

    Initial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _puts_recursion - prints a string, followed by a new line
+*
+* @s: input string
+*
+*/
+
+
+void _puts_recursion(char *s)
+{
+	int startIndex = 0;
+	int *index = &startIndex;
+
+	if (*(s + *(index) != '\0')
+	{
+		putchar(*(s + 0));
+		_puts_recursion(s + *index + 1);
+	}
+	putchar('\n');
+}
