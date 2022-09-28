726d267070174fd6a623e1f3a692bdaa46ae49dd
Joseph Abroquah <abroquahjoseph@gmail.com>
2022-09-28 14:49:28 +0000

    Updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
 * _puts_recursion - prints a string, followed by a new line
@@ -18,5 +19,9 @@ void _puts_recursion(char *s)
 	{
 		putchar(*(s + 0));
 		_puts_recursion(s + *index + 1);
+	}
+	if (*index == strlen(s) - 1)
+	{
+		putchar('\n');
 	}
 }
